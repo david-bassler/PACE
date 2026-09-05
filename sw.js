dd8e4c1a4b5405a7b329f933bcf0655cfb12d1ec
david-bassler <davi.d.h.bassler@gmail.com>
2026-09-05 10:10:19 +0200

    fix(pwa): refresh cache for emoji picker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pace-v18';
+const CACHE = 'pace-v19';
 const ASSETS = [
   './', './index.html', './style.css', './manifest.webmanifest', './icon.svg', './icon-maskable.svg',
   './js/main.js', './js/core/storage.js', './js/core/ui.js', './js/core/google.js', './js/core/sync.js',
