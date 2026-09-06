ae73259da54b7d05917828e5991224c0a0e5b896
david-bassler <davi.d.h.bassler@gmail.com>
2026-09-06 14:14:43 +0200

    fix(pwa): refresh cache for IndexedDB storage

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pace-v24';
+const CACHE = 'pace-v25';
 const ASSETS = [
   './', './index.html', './style.css', './manifest.webmanifest', './icon.svg', './icon-maskable.svg',
   './js/main.js', './js/core/storage.js', './js/core/ui.js', './js/core/google.js', './js/core/sync.js',
